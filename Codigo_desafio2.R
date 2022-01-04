2dcf38b809753444456b7adac1ca9b48b5bde6f1
JoaoPedroFM <jpfmartins@id.uff.br>
2022-01-04 11:05:29 -0300

    Update Codigo_desafio2.R

--- Codigo_desafio2.R
+++ Codigo_desafio2.R
@@ -41,7 +41,7 @@ hist(log(base$premio),main="",xlab="log(premio)")
 base$log_premio <- log(base$premio)
 
 # Transformando em geodata (para usar as funcoes do pacote geoR)
-dados <- as.geodata(base, coords.col = 8:7, data.col = 17, covar.col=c(1:6,9:16))
+dados <- as.geodata(base, coords.col = 3:2, data.col = 4, covar.col=1)
 
 plot(dados)
 
@@ -136,85 +136,3 @@ par(mfrow=c(1,1),mar=c(4,4,0.5,0.5))
 image(krigagem, loc=grid.pred, coords=dados$coords,
       values=krigagem$krige.var, col=terrain.colors(256))
 plot(mapaRJ,xlab="longitude",ylab="latitude",add=TRUE)
-
-
-
-
-
-########################
-### Modelo Bayesiano ###
-########################
-# FREQUENTISTA
-
-# Gerando os dados
-dados <- grf(100, cov.pars=c(10, .4),cov.model="spherical",nug=1)
-mv <- likfit(dados, ini=c(10,0.4), nug=1,cov.model="spherical")
-
-
-plot(dados$coords,pch=16)
-
-variograma=variog(dados)
-plot(variograma,col="blue")
-lines(mv,lty=1,col="blue")
-
-
-# Definindo a malha de pontos para predicao
-grid <- expand.grid(seq(0, 1, l=30), seq(0, 1, l=30))
-
-plot(dados$coords,pch=16)
-points(grid,pch=16,col=2)
-
-krigagem <- krige.conv(dados, loc=grid, krige=krige.control(type.krige="SK", trend.d="cte",beta =mv$beta, cov.model="spherical",cov.pars=mv$cov.pars,
-                                                            nugget=mv$nugget))
-
-
-image(krigagem, loc=grid, coords=dados$coords, col=terrain.colors(256),
-      x.leg=c(0.8,0.85), y.leg=c(0.6,0.9),cex.leg=0.7,vertical=TRUE)
-
-image(krigagem, loc=grid, coords=dados$coords, col=terrain.colors(256),
-      values=krigagem$krige.var, x.leg=c(0.8,0.85), y.leg=c(0.6,0.9),cex.leg=0.7,vertical=TRUE)
-
-
-# BAYESIANO
-
-# Fazendo inferencia bayesiana
-bayes <- krige.bayes(dados, loc=grid, model = model.control(cov.model = "spherical"),
-                     prior = prior.control(beta.prior = "flat", sigmasq.prior = "reciprocal",
-                                           phi.prior="uniform", phi.discrete=seq(0,1,l=10),
-                                           tausq.rel.prior = "uniform",
-                                           tausq.rel.discrete = seq(0, 1, l=10)))
-
-# Priori e posteriori para o parametro phi e tau^2/sigma2
-par(mfrow=c(1,2))
-plot(bayes, phi.dist = TRUE, tausq.rel.dist = TRUE, type="bars",col=c("red","blue"))
-par(mfrow=c(1,1))
-
-# Histogramas da Posteriori
-par(mfrow=c(2,2))
-hist(bayes,density.est = T)
-par(mfrow=c(1,1))
-
-# Plotando o variograma empirico e algumas estimativas bayesianas:
-
-# Variograma empirico
-plot(variog(dados))
-
-# Variograma empirico com o verdadeiro valor do variograma no grafico
-plot(dados)
-
-# Adicionando a media, mediana e moda a posteriori ao variograma
-lines(bayes, summ = mean, lwd=1, lty=2,col=1)
-lines(bayes, summ = median, lwd=2, lty=2,col=2)
-lines(bayes, summary = "mode", post = "parameters",col=3)
-
-# Plotando o variograma empirico e adicionando a mediana e quantis estimados
-plot(variog(dados),ylim=c(0,20))
-quantis <- function(x){quantile(x, prob = c(0.05,0.5, 0.95))}
-lines(bayes, summ = quantis, ty="l", lty=c(2,1,2), col=c(4,2,4))
-
-# Predicao
-image(bayes, main="Valores preditos",col=terrain.colors(256),x.leg=c(0.85,0.9), y.leg=c(0.8,1), cex.leg=0.9,vertical=TRUE)
-# points(dados, cex.min=1, cex.max=4, pt.sizes="deciles",col=terrain.colors(10),add=T)
-
-# Variancia de Predicao
-image(bayes, val="variance", main="Variancia de predicao",col=terrain.colors(256),x.leg=c(0.85,0.9), y.leg=c(0.8,1), cex.leg=0.9,vertical=TRUE)
